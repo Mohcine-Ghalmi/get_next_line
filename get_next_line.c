9acb22fe58f0fe7f9b547ac2e0c6a91c8d2b5f79
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-11-03 13:42:42 +0100

    commit

--- get_next_line.c
+++ get_next_line.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:12:03 by mghalmi           #+#    #+#             */
-/*   Updated: 2022/10/20 12:06:28 by mghalmi          ###   ########.fr       */
+/*   Updated: 2022/11/03 13:42:27 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
+// char	*get_next_line(int fd)
+// {
+	
+// }
 
+//count lines
+int main()
+{
+	int fd;
+	char tab[1000];
+	
+	fd = open("text.txt", O_RDONLY);
+	read(fd, tab, 5);
+	printf("%s" , tab);
 }
+
+// size_t count_lines(int fd)
+// {
+	
+// }
