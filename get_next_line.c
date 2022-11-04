79d3ec04f33b2c0ad931eee5e5c5270e48eb82a6
Mohcine-Ghalmi <mohcin.ghalmi@gmail.com>
2022-11-04 17:48:24 +0100

    test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mghalmi <mghalmi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 10:12:03 by mghalmi           #+#    #+#             */
-/*   Updated: 2022/11/04 16:34:41 by mghalmi          ###   ########.fr       */
+/*   Updated: 2022/11/04 16:57:54 by mghalmi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*line(int fd, char *static_buffer)
 {
 	char	*buffer;
-	int		size;
+	int	size;
 
 	size = 1;
 	buffer = (char *)malloc(sizeof(char) *  (BUFFER_SIZE + 1));
@@ -24,6 +24,11 @@ char	*line(int fd, char *static_buffer)
 	while (size != 0 && ft_strchr(buffer , '\n') == NULL)
 	{
 		size = read(fd, buffer, BUFFER_SIZE);
+		if (size == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
 		buffer[size] = '\0';
 		static_buffer = ft_strjoin(static_buffer, buffer);
 	}
@@ -44,7 +49,7 @@ char	*fixed_line(int fd, char *static_buffer)
 
 char	*get_next_line(int fd)
 {
-	char *static_buffer;
+	static char	*static_buffer;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -57,6 +62,8 @@ int main()
 	int fd;
 	char *static_buffer;
 	fd = open("text.txt" , O_RDONLY);	
-	static_buffer = get_next_line(fd);
+	static_buffer = fixed_line(fd, static_buffer);
+	printf("%s\n" , static_buffer);
+	static_buffer = fixed_line(fd, static_buffer);
 	printf("%s\n" , static_buffer);
 }
