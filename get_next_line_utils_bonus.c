0925b5d324c14e9e345df01df410eda44a2502c2
Mohcine-Ghalmi <81354228+Mohcine-Ghalmi@users.noreply.github.com>
2022-11-13 10:16:46 +0100

    Update get_next_line_utils_bonus.c

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*ft_strjoin(char *left_str, char *buff)
 {
